f33ddb899eee4c24d2125cbc4ecbba3ae6c1d861
mercybii <mercybii95@gmail.com>
2023-06-07 22:33:36 +0300

    all

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for j in range(122, 96, -1):
-    if (j % ) != 0:
+    if (j % 2) != 0:
         j -= 32
         print("{}".format(chr(j)), end="")
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    
+
     j = 0
     str_cpy = ""
     for character in str[:]:
