44536eff84d40c9cf701cb5bb3ff036a2bb3be7c
mercybii <mercybii95@gmail.com>
2023-06-07 22:27:26 +0300

    all

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for j in range(122, 96, -1):
+    if (j % ) != 0:
+        j -= 32
+        print("{}".format(chr(j)), end="")
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    
+    j = 0
+    str_cpy = ""
+    for character in str[:]:
+        if j != n:
+            str_cpy += character
+            j += 1
+            return str_cpy
