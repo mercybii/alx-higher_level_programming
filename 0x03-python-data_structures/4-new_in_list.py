7e90322e7128643e5bedda9894971fb13f3dcea2
mercybii <mercybii95@gmail.com>
2023-06-11 22:30:36 +0300

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,5 +6,5 @@ def new_in_list(my_list, idx, element):
     for ele in my_list:
         if i == idx:
             new_list[i] = element
-            i += 1
-            return (new_list)
+        i += 1
+    return (new_list)
