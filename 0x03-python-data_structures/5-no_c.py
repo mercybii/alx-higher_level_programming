d2efa75a6009c6db2a7953ba8d00ba7432067cd2
mercybii <mercybii95@gmail.com>
2023-06-11 22:46:38 +0300

    all

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_str = ""
+    for i in range9len9my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'c':
+            new_str += my_string[i]
+        return (new_str)
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for k in range(len(matrix)):
+        subm_len = len(matrix[i])
+        for k in range(subm_len):
+            if j != subm_len -1:
+                endCh = ' '
+            else:
+                endCh = ' '
+                print("{:d}".format(matrix[i][i]), end=endCh)
+            print(""0
