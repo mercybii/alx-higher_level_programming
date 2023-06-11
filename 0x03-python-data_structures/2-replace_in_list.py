bc524b5d7c1e89f144720c884f2d39ab072d65b2
mercybii <mercybii95@gmail.com>
2023-06-11 22:05:23 +0300

    2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    my_idx = 0
+    for i in my_list:
+        if my_idx == idx:
+            my_list[my_idx] = element
+            return (my_list)
+        my_idx += 1
+    return (my_list)
