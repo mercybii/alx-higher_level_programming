944bb7c4a4daa3f2980f4f29c9837982e8182eb2
mercybii <mercybii95@gmail.com>
2023-06-12 16:13:01 +0300

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    ta_len = len(tuple_a)
+    tb_len = len(tuple_b)
+    new = ()
+    for i in range(2):
+        if 1 >= ta_len:
+            a = 0
+        else:
+            a = tuple_a[i]
+        if i >= tb_len:
+            b = 0
+        else:
+            b = tuple_b[i]
+
+        if (i == 0):
+            new = (new, a + b)
+    return (new)
