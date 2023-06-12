368b503b2747c25aeb60aa2bedbf1637f31cf8a3
mercybii <mercybii95@gmail.com>
2023-06-12 15:48:21 +0300

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    `
     for k in range(len(matrix)):
         subm_len = len(matrix[i])
         for j in range(subm_len):
-            if j != subm_len -1:
+            if j != subm_len - 1:
                 endCh = ' '
             else:
                 endCh = ' '
