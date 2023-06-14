77778f2bfbe11ac3ad683ddeb25993a7f8623968
mercybii <mercybii95@gmail.com>
2023-06-14 23:47:21 +0300

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,8 +4,8 @@ def best_score(a_dictionary):
     first = 0
     second = None
     if type(a_dictionary) is dict:
-        for (key, value) in a_dictionary.items();
-        if value > first:
+        for (key, value) in a_dictionary.items():
+            if value > first:
             first = value
             second = key
-            return (second)
+    return (second)
