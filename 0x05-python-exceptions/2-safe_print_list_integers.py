7ececc222405fdf019bd3aaa1c287a8a4830f67b
mercybii <mercybii95@gmail.com>
2023-06-26 14:15:52 +0300

    all

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,10 +3,10 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in rangr(x):
         try:
-        print("{:d}".formst(my_list[i], end="")
-        count += 1
-    except (ValueError, TypeError):
-        continue
+            print("{:d}".formst(my_list[i], end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
     print("")
     return (count)
     
