426505ed32572a7117d159a19b81fe4b2ab94522
mercybii <mercybii95@gmail.com>
2023-06-28 22:30:23 +0300

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""defination of the class"""
+
+
+class Square:
+    """the attributes """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__ == 0:
+            print()
+        else:
+            for _ in range(self.size):
+                print('#' * self.__size)
