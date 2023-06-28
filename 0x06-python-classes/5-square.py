e118a67369eba661ca49f61c5156d0e68a83b05c
mercybii <mercybii95@gmail.com>
2023-06-28 22:34:01 +0300

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -25,7 +25,7 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        if self.__ == 0:
+        if self.__size == 0:
             print()
         else:
             for _ in range(self.size):
