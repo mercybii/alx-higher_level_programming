edf3f3fdf5f227c3875773c1403934948fc399cb
mercybii <mercybii95@gmail.com>
2023-06-28 14:55:21 +0300

    all

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 """class squre that define a squre"""
 
 
 class square:
-"""the defination of the attributes"""
+    """the defination of the attributes"""
     def __init__(self, size):
         self.__size = size
