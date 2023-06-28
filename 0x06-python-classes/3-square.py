4f93f63851d9e0962ebdb4f481126fca836cf3ff
mercybii <mercybii95@gmail.com>
2023-06-28 15:57:59 +0300

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class that define a sqare"""
+
+
+class Square:
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise TypeError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """calculate the square area
+
+        Return:
+        the area of the square
+        """
+        return (self.__size)
