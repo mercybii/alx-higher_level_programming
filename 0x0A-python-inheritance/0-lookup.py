71c76dc91b44d2de349579bcee0e81a5d01f8712
mercybii <mercybii95@gmail.com>
2023-07-10 12:08:27 +0300

    all

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,5 +7,9 @@ is a function that look for attributes
 def lookup(obj):
     """
     return list of objects
+
+    obj: are the object to insert
+    
+    return: list of available attributes and methods
     """
     return dir(obj)
