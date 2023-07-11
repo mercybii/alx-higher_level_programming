2ecd481297c6119bcf4f4ad36e12da77862093ea
mercybii <mercybii95@gmail.com>
2023-07-11 23:56:51 +0300

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function that return the dictonary with simple data structure
+"""
+
+
+def class_to_json(obj):
+    """
+    return the dictionary with simple
+    data stracture
+    """
+    return obj.__dict__
