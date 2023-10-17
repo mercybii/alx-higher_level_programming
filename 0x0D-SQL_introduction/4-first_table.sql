31af41f204a9885d0a2cf6f60d864176b1a877af
mercybii <mercybii95@gmail.com>
2023-10-17 23:01:21 +0300

    all

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- create table in the current database
 
-CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table ('id' INT, 'name' VARCHAR(256));
