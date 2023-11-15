aa1ddfa36bb29f3d7d3e3f8b9b00c83d30f9e28f
mercybii <mercybii95@gmail.com>
2023-11-15 19:40:59 +0300

    4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+//print two agument using console.log
+
+const firstArgs = process.argv[2];
+const secondArgs = process.argv[3];
+
+//print console.log
+
+console.log(firstArgs + 'is' + secondArgs);
