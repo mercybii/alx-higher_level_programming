6e9479bf25aaf31e14d01c33c45e5c23f83da70b
mercybii <mercybii95@gmail.com>
2023-11-20 19:27:33 +0300

    all

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+List all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Get command line arguments
+    """
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    """
+    Connect to the database
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    """
+    Create a cursor object
+    """
+    cursor = db.cursor()
+
+    """
+    Execute the query
+    """
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+
+    """
+    Fetch all the rows
+    """
+    rows = cursor.fetchall()
+
+    """
+    Display the results
+    """
+    for row in rows:
+        print(row)
+
+    """
+    Close the cursor and database connection
+    """
+    cursor.close()
+    db.close()
